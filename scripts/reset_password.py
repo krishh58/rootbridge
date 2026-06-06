5e9762db826ab11c19051287510a424b12dc1986
K. Henderson <krishndrsn@gmail.com>
2026-06-06 14:42:08 -0400

    chore: add password reset script

---
+++ scripts/reset_password.py
@@ -0,0 +1,14 @@
+import bcrypt
+from app import create_app
+from app.db import db
+from app.models import User
+
+app = create_app()
+with app.app_context():
+    user = User.query.filter_by(email='krishndrsn@gmail.com').first()
+    if user:
+        user.password_hash = bcrypt.hashpw(b'RootBridge2026!', bcrypt.gensalt()).decode()
+        db.session.commit()
+        print('Password reset to: RootBridge2026!')
+    else:
+        print('User not found')
